fec2bdc7e14065b3ed4809f056bba2060858f8fc
Arsenia <arsenia1993@mail.ru>
2022-10-30 11:57:33 +0300

    Добавила решение задачи

---
+++ Itog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+string[] massive = { "Life", "is", "just", "a", "joke" };
+int i = 0;
+int size = 0;
+
+while (massive.Length > i)
+{
+   if (massive[i].Length < 4 && massive[i].Length > 0)    
+        size++;
+        Console.Write("{0} ", massive[i]);
+        i++;
+}
+Console.WriteLine();
+string[] massive2 = new string[size];
+i = 0;
+int j = 0;
+while (massive.Length > i)
+{
+    if (massive[i].Length < 4 && massive[i].Length > 0)    
+    {
+        massive2[j] = massive[i];
+        Console.Write("{0} ", massive2[j]);
+        j++;
+    }
+    i++;
+}
+
